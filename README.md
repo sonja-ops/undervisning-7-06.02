54b5acbe53fbb374158d84dabce217134cb62437
sonja-ops <sodju@hotmail.com>
2025-02-06 10:09:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# undervisning-7-06.02
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/sonja-ops/undervisning-7-06.02)
---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+Datatyper
+
+Oppgaver
+1. Lag variabel name, age, isLearningJs, putt inn forskjellige datatyper om deg selv
+2. Console.log hver variabel
+3. Bruk console log for å finne datatypen til en variabel
+4. Lag en array av forskjellige ingredienser til en bakeoppskrift
+
+Funksjon
+
+Oppgaver
+
+1. lag en funksjon som console.log 'Hello World'
+2. Lag en funksjon som legger sammen 2 tall
+3. Lag en funksjon med en variabel som har en array og console.log arrayet
+
+
+*/
